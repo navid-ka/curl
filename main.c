2bf9690e73466efdc4fc897a52c6210d09c92e89
Bifrost <nkeyani-@student.42barcelona.com>
2024-02-24 13:27:29 +0100

    now can download images

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ typedef struct curl_write_callback {
   size_t size;
 } t_callback;
 
+/*
 size_t write_callback(void *ptr, size_t size, size_t nmemb, void *data) {
   size_t realsize = size * nmemb;
   t_callback *mem = (t_callback *)data;
@@ -18,13 +19,14 @@ size_t write_callback(void *ptr, size_t size, size_t nmemb, void *data) {
     mem->data[mem->size] = 0;
   }
   return realsize;
-}
+} */ // This only works for text files and not for binary files
 
+/*
 void write_to_file(FILE *file, char *data) {
   if (file) {
     fprintf(file, "%s", data);
   }
-}
+} */ // This only works for text files and not for binary files
 
 int main(int argc, char **argv) {
 
@@ -36,7 +38,7 @@ int main(int argc, char **argv) {
   chunk.data = malloc(1);
   chunk.size = 0;
   char *url = argv[1];
-  FILE *file = fopen(argv[2], "w");
+  FILE *file = fopen(argv[2], "wb");
   CURL *curl;
   CURLcode res;
   curl = curl_easy_init();
@@ -47,15 +49,16 @@ int main(int argc, char **argv) {
   }
   if (curl) {
     curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &chunk);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL); 
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
     res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
       fclose(file);
     }
-    write_to_file(file, chunk.data);
+    // write_to_file(file, chunk.data); Not working for binary files
     curl_easy_cleanup(curl);
   }
   free(chunk.data);
